f9b78e3149179cd01f604079f0666e8211583b56
calvin-c13 <calvinwchau@gmail.com>
2024-11-07 22:15:53 -0500

    made express east bus tracker marker bigger

--- src/utils/mapSetup.js
+++ src/utils/mapSetup.js
@@ -107,10 +107,10 @@ const leafletHTML = `
         //Bus Icons
         var busExpressEast = new L.Icon({
           iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
-          iconSize: [22, 37],
-          iconAnchor: [11, 37],
-          popupAnchor: [1, -30],
-          shadowSize: [40, 40]
+          iconSize: [26, 44],
+          iconAnchor: [13, 44],
+          popupAnchor: [1, -34],
+          shadowSize: [44, 44]
         });
 
         var busExpressWest = new L.Icon({
