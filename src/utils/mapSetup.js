bd6c9deb1cf908cab1d489daae1557f16d531f89
calvin-c13 <calvinwchau@gmail.com>
2024-11-07 22:21:36 -0500

    made inner bus tracker marker bigger

--- src/utils/mapSetup.js
+++ src/utils/mapSetup.js
@@ -139,10 +139,10 @@ const leafletHTML = `
 
         var busInner = new L.Icon({ 
           iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png', 
-          iconSize: [22, 37],
-          iconAnchor: [11, 37],
-          popupAnchor: [1, -30],
-          shadowSize: [40, 40]
+          iconSize: [26, 44],
+          iconAnchor: [13, 44],
+          popupAnchor: [1, -34],
+          shadowSize: [44, 44]
         });
 
         var busOuter = new L.Icon({ 
