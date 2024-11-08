d3f108682d4ca01751f64ed91c0d4a9acb380b4f
calvin-c13 <calvinwchau@gmail.com>
2024-11-07 22:22:09 -0500

    made railroad bus tracker marker bigger

--- src/utils/mapSetup.js
+++ src/utils/mapSetup.js
@@ -155,10 +155,10 @@ const leafletHTML = `
 
         var busRailroad = new L.Icon({ 
           iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-black.png',
-          iconSize: [22, 37],
-          iconAnchor: [11, 37],
-          popupAnchor: [1, -30],
-          shadowSize: [40, 40]
+          iconSize: [26, 44],
+          iconAnchor: [13, 44],
+          popupAnchor: [1, -34],
+          shadowSize: [44, 44]
         });
 
         var busIcon = new L.Icon({ 
